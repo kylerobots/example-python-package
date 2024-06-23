5b48b327ce473919ebfa49f51619fd350c1968ed
Kyle M. Hart <kylemhart@gmail.com>
2024-06-23 15:58:54 -0400

    fix: drop support for Python 3.9

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@
 import nox
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.10", "3.11", "3.12"])
 @nox.parametrize(arg_names="numpy", arg_values_list=["1.26.4", "2.0.0"])
 def tests(session: nox.Session, numpy: str) -> None:
     """Run unit tests on different versions of NumPy and Python.
